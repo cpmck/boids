b7fd1e74f4daa8331d6cd9da3929f3ec9ceaa2fc
Conor Mc Keever <conormckeever92@gmail.com>
2017-02-21 15:59:46 +0000

    REFACTORING_2: started changing over to numpy arrays. Initial random positions now in arrays

--- boids/badboids.py
+++ boids/badboids.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt 
 from matplotlib import animation
 import random
-
+import numpy as np
 
 #initial poor code
 x_init_pos_min = -1000.0
@@ -19,12 +19,20 @@ y_init_vel_max = 20
 
 num_boids = 50
 
+
+boids_x_np = np.random.uniform(x_init_pos_min, x_init_pos_max, num_boids)
+boids_y_np = np.random.uniform(y_init_pos_min, y_init_pos_max, num_boids)
+
+
 boids_x = [random.uniform(x_init_pos_min,x_init_pos_max) for x in range(num_boids)]
 boids_y = [random.uniform(y_init_pos_min,y_init_pos_max) for x in range (num_boids)]
 boid_x_velocities = [random.uniform(x_init_vel_min,y_init_vel_max) for x in range(num_boids)]
 boid_y_velocities = [random.uniform(y_init_vel_min,y_init_vel_max) for x in range(num_boids)]
 boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
+
+#print(boids_x_np)
+#print(boids_x)
 #boids is a set of initial positions and velocities
 
 middle_strength = 0.001
@@ -36,6 +44,12 @@ retreat_dist = 10
 def update_boids(boids, middle_strength, match_strength, num_boids):
 	xs,ys,xvs,yvs = boids
 	#fly towards middle
+
+
+
+
+
+
 	for i in range(num_boids):
 		for j in range(num_boids):
 			xvs[i] = xvs[i] + ( xs[j] - xs[i])*middle_strength/len(xs)
@@ -75,7 +89,7 @@ def animate(frame):
 	update_boids(boids,middle_strength, match_strength, num_boids)
 	scatter.set_offsets(zip(boids[0],boids[1]))
 
-anim = animation.FuncAnimation(figure, animate, frames = 500, interval = 50)
+anim = animation.FuncAnimation(figure, animate, frames = 3, interval = 50)
 
 if __name__ == "__main__":
 	plt.show()
