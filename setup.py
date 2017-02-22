f788e39299a1b46f078fb8c9d7d345fd26784410
Conor Mc Keever <conormckeever92@gmail.com>
2017-02-22 18:08:26 +0000

    added config file

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
 		version = '0.1.0',
 		packages = find_packages(exclude=['test']),
 		scripts = ['scripts/flyBoids'],
+		data_files = ["config/config.yaml"],
 		install_requires = ['numpy','matplotlib'] #add all packages
 
 )
