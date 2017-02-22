0241fd07374b46a8d9c602f676274f3a185e71ed
Conor Mc Keever <conormckeever92@gmail.com>
2017-02-22 18:06:08 +0000

    added config file

--- boids/betterboids.py
+++ boids/betterboids.py
@@ -6,32 +6,34 @@ from flock import Flock
 from flight import Flight
 import yaml
 
+def process():
 
-config = yaml.load(open("config.yaml"))
+	config = yaml.load(open("config.yaml"))
 
-num_boids = config["number_of_boids"]
-initial_params = np.array([config["x_bounds"], config["y_bounds"]])
+	num_boids = config["number_of_boids"]
+	initial_params = np.array([config["x_bounds"], config["y_bounds"]])
 
-interaction_params = np.array([config["centre_attraction"], config["retreat_distance"], config["attraction_distance"], config["drag_strength"]])
+	interaction_params = np.array([config["centre_attraction"], config["retreat_distance"], config["attraction_distance"], config["drag_strength"]])
 
-frame_x_min = config["x_bounds"][0]-config["x_margin"]
-frame_x_max = config["x_bounds"][1]+config["x_margin"]
+	frame_x_min = config["x_bounds"][0]-config["x_margin"]
+	frame_x_max = config["x_bounds"][1]+config["x_margin"]
 
-frame_y_min = config["y_bounds"][0]-config["y_margin"]
-frame_y_max = config["y_bounds"][1]+config["y_margin"]
+	frame_y_min = config["y_bounds"][0]-config["y_margin"]
+	frame_y_max = config["y_bounds"][1]+config["y_margin"]
 
 
-flock_of_boids = Flock(num_boids, initial_params).init_cond_matrix()
-flying_flock = Flight(flock_of_boids,interaction_params)
+	flock_of_boids = Flock(num_boids, initial_params).init_cond_matrix()
+	flying_flock = Flight(flock_of_boids,interaction_params)
 
-figure = plt.figure()
-axes = plt.axes(xlim=(frame_x_min, frame_x_max), ylim = (frame_y_min, frame_y_max))
-scatter = axes.scatter(flying_flock.get_x(),flying_flock.get_y())
+	figure = plt.figure()
+	axes = plt.axes(xlim=(frame_x_min, frame_x_max), ylim = (frame_y_min, frame_y_max))
+	scatter = axes.scatter(flying_flock.get_x(),flying_flock.get_y())
 
-def animate(frame):
-	scatter.set_offsets(zip(flying_flock.update_boids().get_x(), flying_flock.update_boids().get_y()))
+	def animate(frame):
+		scatter.set_offsets(zip(flying_flock.update_boids().get_x(), flying_flock.update_boids().get_y()))
 
-anim = animation.FuncAnimation(figure, animate, frames = config["frame_number"], interval = config["frame_interval"])
+	return animation.FuncAnimation(figure, animate, frames = config["frame_number"], interval = config["frame_interval"])
+
 
 if __name__ == "__main__":
 	plt.show()
--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
 		version = '0.1.0',
 		packages = find_packages(exclude=['test']),
 		scripts = ['scripts/flyBoids'],
-		install_requires = ['numpy'] #add all packages
+		install_requires = ['numpy','matplotlib'] #add all packages
 
 )
