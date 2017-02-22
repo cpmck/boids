b1627adebc4d222fe6624f3ade1eb82dbba76f6c
Conor Mc Keever <conormckeever92@gmail.com>
2017-02-22 17:23:32 +0000

    REFACTORING_10 changed some variable names

--- boids/badboids.py
+++ boids/badboids.py
@@ -12,20 +12,7 @@ config = yaml.load(open("config.yaml"))
 num_boids = config["number_of_boids"]
 initial_params = np.array([config["x_bounds"], config["y_bounds"]])
 
-attract_dist = 100
-middle_strength = 0.001
-match_strength = 0.125
-retreat_dist = 10
-
 interaction_params = np.array([config["centre_attraction"], config["retreat_distance"], config["attraction_distance"], config["drag_strength"]])
-
-x_margin = 1000
-y_margin = 1000
-
-boids_flock = Flock(50, initial_params)
-flock_matrix = boids_flock.init_cond_matrix()
-flight_matrix = Flight(flock_matrix,interaction_params)
-
 
 frame_x_min = config["x_bounds"][0]-config["x_margin"]
 frame_x_max = config["x_bounds"][1]+config["x_margin"]
@@ -34,14 +21,15 @@ frame_y_min = config["y_bounds"][0]-config["y_margin"]
 frame_y_max = config["y_bounds"][1]+config["y_margin"]
 
 
+flock_of_boids = Flock(num_boids, initial_params).init_cond_matrix()
+flying_flock = Flight(flock_of_boids,interaction_params)
+
 figure = plt.figure()
 axes = plt.axes(xlim=(frame_x_min, frame_x_max), ylim = (frame_y_min, frame_y_max))
-scatter = axes.scatter(flock_matrix[:,0],flock_matrix[:,1])
-
-
+scatter = axes.scatter(flying_flock.get_x(),flying_flock.get_y())
 
 def animate(frame):
-	scatter.set_offsets(zip(flight_matrix.update_boids().get_x(), flight_matrix.update_boids().get_y()))
+	scatter.set_offsets(zip(flying_flock.update_boids().get_x(), flying_flock.update_boids().get_y()))
 
 anim = animation.FuncAnimation(figure, animate, frames = 50, interval = 50)
 
--- boids/flight.py
+++ boids/flight.py
@@ -7,11 +7,9 @@ class Flight(object):
 	def fly_towards_middle(self):
 
 		middle_strength = self.interaction_params[0]
-		num_boids = len(self.boids[:,0])
-
-		for i in range(num_boids):
-			self.boids[:,2] = self.boids[:,2] + (self.boids[i,0] - self.boids[:,0] )*middle_strength/len(self.boids[:,0])
-			self.boids[:,3] = self.boids[:,3] + (self.boids[i,1] - self.boids[:,1] )*middle_strength/len(self.boids[:,1])
+		for i in range(self.num_boids()):
+			self.boids[:,2] = self.boids[:,2] + (self.boids[i,0] - self.boids[:,0] )*middle_strength/self.num_boids()
+			self.boids[:,3] = self.boids[:,3] + (self.boids[i,1] - self.boids[:,1] )*middle_strength/self.num_boids()
 
 		return self
 
@@ -19,27 +17,24 @@ class Flight(object):
 	def fly_apart(self):
 
 		fly_apart_range = self.interaction_params[1]
-		num_boids = len(self.boids[:,0])
 
-		for i in range(num_boids):
+		for i in range(self.num_boids()):
 			target_indices = ( self.boids[i,0]-self.boids[:,0] )**2 + ( self.boids[i,1]-self.boids[:,1] )**2 < fly_apart_range
 			self.boids[:,2] = self.boids[:,2] + ( self.boids[:,0] - self.boids[i,0] )*target_indices.astype(float)
 			self.boids[:,3] = self.boids[:,3] + ( self.boids[:,1] - self.boids[i,1] )*target_indices.astype(float)
 
 		return self
-
-
+		
 
 	def fly_together(self):
 
 		fly_together_range = self.interaction_params[2]
 		together_strength = self.interaction_params[3]
-		num_boids = len(self.boids[:,0])
 
-		for i in range(num_boids):
+		for i in range(self.num_boids()):
 			target_indices = ( self.boids[i,0]-self.boids[:,0] )**2 + ( self.boids[i,1]-self.boids[:,1] )**2 < fly_together_range
-			self.boids[:,2] = self.boids[:,2] + ( self.boids[:,2] - self.boids[i,2] )*together_strength/num_boids*target_indices.astype(float)
-			self.boids[:,3] = self.boids[:,3] + ( self.boids[:,3] - self.boids[i,3] )*together_strength/num_boids*target_indices.astype(float)
+			self.boids[:,2] = self.boids[:,2] + ( self.boids[:,2] - self.boids[i,2] )*together_strength/self.num_boids()*target_indices.astype(float)
+			self.boids[:,3] = self.boids[:,3] + ( self.boids[:,3] - self.boids[i,3] )*together_strength/self.num_boids()*target_indices.astype(float)
 
 		return self
 
