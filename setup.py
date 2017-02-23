aa66e2132c664d68bd5a87afc6f170995e184f84
Conor Mc Keever <conormckeever92@gmail.com>
2017-02-23 17:37:04 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
 		version = '0.1.0',
 		packages = find_packages(exclude=['test']),
 		scripts = ['scripts/flyBoids'],
-		data_files = ["boids/config/config.yaml"],
+		package_data={
+        '': ['*.yaml','*yml'],},
 		install_requires = ['numpy','matplotlib','pyyaml'], #add all packages
-		incluse_package_data = True
+		incluse_package_data = 'True'
 
 )
